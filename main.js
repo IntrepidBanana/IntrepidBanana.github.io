8a3a2f57678e87596a74fbea2a71fb7df791fce2
Lauris <lpetlah@gmail.com>
2021-02-09 03:20:14 -0500

    style update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const articles = document.querySelectorAll("article");
 articles.forEach(a=>{
     let img = a.querySelector("img");
-    a.style.backgroundImage = `url(${img.src})`;
+    // a.style.backgroundImage = `url(${img.src})`;
 })
